c3ea766c2565962608e1f1270ba8bf8d42448f5c
Ramya <mutyamramya0101@gmail.com>
2023-04-17 20:59:53 +0530

    solution for encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(){
+      RWOnly obj=new RWOnly();
+     // obj.name="uuu";
+      //System.out.println(obj.name);
+      //name has private access in com.driver.RWOnly
+      obj.setName("Ramya");
+      obj.getName();
+  }
+
 }
